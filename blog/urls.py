20fbb1575e436e6ddc7bb9bbff86ff894cf4110d
Marshall210 <tymchenko99978@gmail.com>
2019-09-22 12:49:29 +0300

    add templates

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-
+from blog import views
 from blog.views import post_list
 
 urlpatterns = [
     path('', post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+
 ]
