34613b8ab86067a9c49abaeb7c5a95253a60c173
Marshall210 <52254398+Marshall210@users.noreply.github.com>
2019-09-22 11:20:31 +0300

    Add files via upload

--- blog/views.py
+++ blog/views.py
@@ -5,16 +5,16 @@ from datetime import datetime
 
 
 def post_list(request):
-    posts = Post.objects.filter(published_date=datetime.now())
-    context = {}
-    context['posts'] = posts
-    return render(request, 'blog/post_list.html', context)
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
 # Create your views here.
 
 
 '''
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts': posts})
+    posts = Post.objects.filter(published_date=datetime.now())
+    context = {}
+    context['posts'] = posts
+    return render(request, 'blog/post_list.html', context)
 
 '''
